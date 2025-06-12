c294767e99c6504e735fb7b4d59f3f07b2e1b0cd
StefanHoareau <flowme.ai@gmail.com>
2025-06-12 19:26:15 +0200

    Delete flowme_states_detection_v03.py

--- flowme_states_detection_v03.py
+++
@@ -1,7 +0,0 @@
-def detect_flowme_state(text):
-    # Détection simple d'exemple
-    if "fatigue" in text.lower():
-        return "Fatigue"
-    elif "curiosité" in text.lower():
-        return "Curiosité"
-    return "Inconnu"
